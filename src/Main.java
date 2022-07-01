e5ee41a96e00902f016415d6fafd9411f1d0683b
F3548784 Gabriel de Paula da Silva Oliveira <gabriel.pso@bb.com.br>
2022-07-01 18:18:08 -0300

    Primeiro Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/*
+Crie um programa para aplicar uma cadeia de descontos no total da compra.
+
+Os descontos são:
+
+    Se comprar 15 itens ou mais, aplicar 10% de desconto.
+    Se comprar 10 itens e menos de 15 aplicar 8% de desconto.
+    Se comprar 5 itens e menos de 10 aplicar 5% de desconto.
+    Se comprar menos que 5 itens não aplicar desconto.
+
+Ao aplicar o desconto encerre a cadeia de verificações. Deixe o código aberto para que outros tipos de descontos sejam aplicados futuramente.
+
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
